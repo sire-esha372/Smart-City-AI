ef837535cc8570cd81ad76979ced1c8697efcb45
sire-esha372 <sireeshapinnmaraju@gmail.com>
2026-08-07 23:01:04 +0530

    Enable traffic router only

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,7 +1,30 @@
-from fastapi import FastAPI
+from pathlib import Path
 
-app = FastAPI()
+from fastapi import FastAPI
+from fastapi.staticfiles import StaticFiles
+
+from .database.database import engine
+from .database.models import Base
+
+from .routers.traffic import router as traffic_router
+
+Base.metadata.create_all(bind=engine)
+
+BASE_DIR = Path(__file__).resolve().parent
+
+app = FastAPI(
+    title="Smart City AI Platform",
+    version="1.0.0"
+)
+
+app.mount(
+    "/static",
+    StaticFiles(directory=str(BASE_DIR / "static")),
+    name="static"
+)
 
 @app.get("/")
 def home():
-    return {"message": "Backend Working"}
+    return {"message": "Welcome"}
+
+app.include_router(traffic_router)
